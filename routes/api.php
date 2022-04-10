c8d18091bf86e2c8f509db93f5e78b7ac67078a5
Xurliman <jumamuratovahurliman8@gmail.com>
2022-04-10 22:38:37 +0500

    basket zavershit/otmenit

--- routes/api.php
+++ routes/api.php
@@ -26,16 +26,19 @@ Route::middleware('auth:sanctum')->group(function() {
 
     Route::post('/order', [OrderController::class, 'create']);
     Route::get('/single/order/{order_id}', [OrderController::class, 'singleOrder']);
+    Route::get('/view-orders', [OrderController::class, 'viewOrders']);
 
-    Route::get('/single/basket/{basket_id}', [OrderController::class, 'singleBasket']);
     Route::delete('/cancel/basket/{basket_id}', [OrderController::class, 'cancelBasket']);
-    Route::get('/history', [OrderController::class, 'completedHistory']);
     Route::put('/complete/basket/{basket_id}', [OrderController::class, 'completeBasket']);
+    Route::get('/history', [OrderController::class, 'completedHistory']);
+    Route::get('/single/basket/{basket_id}', [OrderController::class, 'singleBasket']);
     Route::get('/all/baskets/{user_id}', [OrderController::class, 'allBasketsOfUser']);
+    Route::get('/view-baskets', [OrderController::class, 'viewAllBaskets']);
 
     Route::post('/make/room', [OrderController::class, 'makeRoom']);
     Route::patch('/edit/room/{room_id}', [OrderController::class, 'editRoom']);
     Route::delete('/delete/room/{room_id}', [OrderController::class, 'deleteRoom']);
+    Route::get('/view-rooms', [OrderController::class, 'viewRooms']);
 
     Route::post('/create/category', [CategoryController::class, 'create']);
     Route::patch('/edit/category/{cat_id}', [CategoryController::class, 'edit']);
@@ -53,7 +56,6 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('/users/getme', [AuthController::class, 'getme']); 
 });
 
-
 Route::post('/users/register', [AuthController::class, 'register']);
 Route::post('/users/login', [AuthController::class, 'login']);
 
@@ -63,7 +65,3 @@ Route::get('/event', [EventsController::class, 'view']);
 Route::get('/product', [ProductController::class, 'view']);
 Route::get('/product/{product_id}', [ProductController::class, 'singleProduct']);
 Route::get('/category/product', [ProductController::class, 'showWithCat']);
-
-Route::get('/view-orders', [OrderController::class, 'viewOrders']);
-Route::get('/view-baskets', [OrderController::class, 'viewAllBaskets']);
-Route::get('/view-rooms', [OrderController::class, 'viewRooms']);
